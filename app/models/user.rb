ab8cd666112a901b757ec5e9a5e7c66ae8f96e8a
Akash <akashgupta@blubirch.com>
2023-12-11 17:52:44 +0530

    ignoring jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,26 +14,26 @@ class User < ApplicationRecord
     self.password == password
   end
 
-  def self.generate_token(user)
-    # Customizing the payload
-    payload = { user_id: user.id }
+  # def self.generate_token(user)
+  #   # Customizing the payload
+  #   payload = { user_id: user.id }
     
-    # Use a strong secret key for encryption
-    secret_key = 'c8fa3cc38ed50b339ed516e5210dbff4dd486cce39467df8b345c278ca12013fb96382ce6ed5deb0bbfce746efe9e625cb86e5016e69e458e702b2202ee8b8ce'
+  #   # Use a strong secret key for encryption
+  #   secret_key = 'c8fa3cc38ed50b339ed516e5210dbff4dd486cce39467df8b345c278ca12013fb96382ce6ed5deb0bbfce746efe9e625cb86e5016e69e458e702b2202ee8b8ce'
     
-    # Encrypt the token
-    token = JWT.encode(payload, secret_key, 'HS256')
+  #   # Encrypt the token
+  #   token = JWT.encode(payload, secret_key, 'HS256')
     
-    # Return the encrypted token
-    token
-  end
+  #   # Return the encrypted token
+  #   token
+  # end
 
-  def self.encode_token
-    user = User.find_by(params[:user_id])
-    # jwt = user.token
-    # header, payload, signature = jwt.split('.')
-    # decoded_payload = Base64.decode64(payload)
+  # def self.encode_token
+  #   user = User.find_by(params[:user_id])
+  #   # jwt = user.token
+  #   # header, payload, signature = jwt.split('.')
+  #   # decoded_payload = Base64.decode64(payload)
 
-  end
+  # end
 
 end

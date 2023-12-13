a6744a87ca0e31fa55481491ea1c1dc4b995ede2
Akash <akashgupta@blubirch.com>
2023-12-13 14:52:12 +0530

    minorissue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::API
   def authenticate_user
     if request.headers['Authorization'].present?
       User.all.each do |user|
-        if user.token == request.headers['Authorization']
-          response.headers['Authorization'] = "Bearer #{@token}"
+        if user.token
+          response.headers['Authorization'] = "Bearer #{user.token}"
           return user.id
         end
         return if user.blank?
